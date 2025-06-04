95ac3c5441dfa5f937bada26b7b3bf02a752219a
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-04 15:29:15 +0300

    Events

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 import { MovieComponent } from '../movie/movie.component';
 import { CommonModule } from '@angular/common';
 import { Movies } from '../movie.datasource';
+import { Movie } from '../movie';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movies',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css'
 })
 export class MoviesComponent {
   title = "Movie List"
   movies= Movies;
+  selectedMovie : Movie | undefined;
 
-  
+  onSelect(movie:Movie) : void{
+    this.selectedMovie = movie;
+    console.log('Seçilen:', movie);
+  }
 }
