7f8368734e03fb28da7f85d13f6f68da667fffac
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-04 19:27:01 +0300

    Angular Services

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MovieComponent } from '../movie/movie.component';
 import { CommonModule } from '@angular/common';
-import { Movies } from '../movie.datasource';
 import { Movie } from '../movie';
 import { FormsModule } from '@angular/forms';
 import { MovieDetailComponent } from '../movie-detail/movie-detail.component';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -14,11 +14,21 @@ import { MovieDetailComponent } from '../movie-detail/movie-detail.component';
 })
 export class MoviesComponent {
   title = "Movie List"
-  movies= Movies;
+  movies:Movie[] | undefined;
   selectedMovie : Movie | undefined;
+
+  constructor(private movieService:MovieService){}
+
+  ngOnInit():void{
+    this.getMovies();
+  }
 
   onSelect(movie:Movie) : void{
     this.selectedMovie = movie;
     console.log('Seçilen:', movie);
+  }
+
+  getMovies():void{
+    this.movies = this.movieService.getMovies();
   }
 }
