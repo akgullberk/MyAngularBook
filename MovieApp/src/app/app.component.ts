f7082110b97cab5c3f3a807b413ebc3136ffb160
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-05 10:31:24 +0300

    Logging Component

--- MovieApp/src/app/app.component.ts
+++ MovieApp/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
+import { LoggingComponent } from './logging/logging.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MoviesComponent],
+  imports: [RouterOutlet,MoviesComponent,LoggingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ MovieApp/src/app/logging/logging.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-logging',
+  imports: [CommonModule],
+  templateUrl: './logging.component.html',
+  styleUrl: './logging.component.css'
+})
+export class LoggingComponent {
+
+  constructor(public loggingService: LoggingService) { }
+
+}
