420f6a9219e22311ffe379f046bedd5ef07b8d86
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-07 18:46:25 +0300

    Angular Modelinin Eklenmesi

---
+++ example/src/app/repositories/repository.model.ts
@@ -0,0 +1,23 @@
+import { SimpleDataSource } from "../datasources/datasource.model";
+import { Product } from "../models/product.model";
+
+export class ProductRepository {
+
+    private dataSource: SimpleDataSource;
+    private products: Product[];
+
+    constructor(){
+        this.dataSource = new SimpleDataSource();
+        this.products = new Array<Product>();
+        this.dataSource.getProducts().forEach(p => this.products.push(p));
+    }
+
+    getProducts(): Product[]{
+        return this.products;
+    }
+
+    getProductsById(id: number): Product | undefined{ {
+        return this.products.find(p => p.id == id);
+    }
+}
+}
