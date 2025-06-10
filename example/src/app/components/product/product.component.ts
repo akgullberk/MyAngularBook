3855db01495bddece69899e72cd542ad4ec97d31
Berk Akgül <berkak_gul51@hotmail.com>
2025-06-10 12:44:02 +0300

    ngIf

--- example/src/app/components/product/product.component.ts
+++ example/src/app/components/product/product.component.ts
@@ -3,16 +3,16 @@ import { ProductRepository } from '../../repositories/repository.model';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SummaryPipe } from '../../summary.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule,FormsModule,SummaryPipe],
+  imports: [CommonModule,FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
 
-  text="Lorem ipsum dolor sit, amet consectetur adipisicing"
+  model: ProductRepository = new ProductRepository();
+
 
 }
